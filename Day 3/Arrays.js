0949dca609197e078946f916dc035d34f9c5b79b
Deepak Raj <54245038+perfect104@users.noreply.github.com>
2020-04-25 14:09:16 +0530

    initial commit
    initial commit

--- Day 3/Arrays.js
+++ Day 3/Arrays.js
@@ -1,8 +1,18 @@
-
 function getSecondLargest(nums) {
     // Complete the function
-    console.log(Math.max(nums));    
-    
+    let max1 = 0;
+    for (const e of nums) {
+        if (max1 < e) {
+            max1 = e;
+        }
+    }
+    let max2 = 0;
+    for(const e of nums) {
+        if(max2 < e && e < max1) {
+            max2 = e;
+        }
+    }
+    return max2    
 }
 const nums =  [2,3,6,6,5];
-getSecondLargest(nums);
+console.log(getSecondLargest(nums));
