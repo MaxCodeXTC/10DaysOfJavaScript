c1f10561dc496e656b75128939d65a5410476d6b
Deepak Raj <54245038+perfect104@users.noreply.github.com>
2020-04-26 09:20:07 +0530

    initial commit

---
+++ Day 6/Dates.js
@@ -0,0 +1,8 @@
+function getDayName(dateString) {
+    let dayName;
+    // Write your code here
+    let dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    let date = new Date(dateString)
+    dayName = date.getDay();
+    return dayNames[dayName];
+}
